3e23e302fdea1a466d29edff430b858c0585cac8
nadeeraka <nadeerakan@gmail.com>
2020-10-25 15:44:06 +0530

    controller add

--- src/main/java/com/nimantha/ABC/API/resources/TestResource.java
+++ src/main/java/com/nimantha/ABC/API/resources/TestResource.java
@@ -1,4 +1,28 @@
 package com.nimantha.ABC.API.resources;
 
+import com.nimantha.ABC.API.domain.Test;
+import com.nimantha.ABC.API.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("api/test")
+
 public class TestResource {
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/{testId}")
+    public ResponseEntity<Test> getTestById(HttpServletRequest request, @PathVariable("testId") Integer testId) {
+        int userId = (Integer) request.getAttribute("userId");
+        Test test = testService.fetchTestById(userId, testId);
+        return new ResponseEntity<>(test, HttpStatus.OK);
+    }
 }
