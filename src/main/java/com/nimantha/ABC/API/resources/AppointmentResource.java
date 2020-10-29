517a3cfcc2812734f9df7f2cfdad98274b9ce34f
nadeeraka <nadeerakan@gmail.com>
2020-10-30 02:36:55 +0530

    init appo

--- src/main/java/com/nimantha/ABC/API/resources/AppointmentResource.java
+++ src/main/java/com/nimantha/ABC/API/resources/AppointmentResource.java
@@ -5,13 +5,11 @@ import com.nimantha.ABC.API.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +19,7 @@ public class AppointmentResource {
 
     @GetMapping("")
     public ResponseEntity<List<Appointment>> getAllAppointments(HttpServletRequest request,
-                                                                        @PathVariable("categoryId") Integer appId) {
+                                                                @PathVariable("categoryId") Integer appId) {
         int userId = (Integer) request.getAttribute("userId");
         List<Appointment> app = appointmentService.fetchAllTest(userId);
         return new ResponseEntity<>(app, HttpStatus.OK);
@@ -29,26 +27,33 @@ public class AppointmentResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Appointment> getAppointmentById(HttpServletRequest request,
-                                                          @PathVariable("id") Integer appId){
+                                                          @PathVariable("id") Integer appId) {
         int userId = (Integer) request.getAttribute("userId");
-       Appointment app =appointmentService.appFindById(userId,appId);
+        Appointment app = appointmentService.appFindById(userId, appId);
         return new ResponseEntity<>(app, HttpStatus.OK);
     }
 
     @PostMapping("")
     public ResponseEntity<Appointment> addAppointment(HttpServletRequest request,
                                                       @PathVariable("id") Integer userId,
-                                                      @RequestBody Map<String, Object> transactionMap) {
+                                                      @RequestBody Map<String, Object> AppMap) {
         int testId = (Integer) request.getAttribute("testId");
-        String description = (String) transactionMap.get("description");
-        Date appointment_time = (Date) transactionMap.get("appTime");
-       int app = appointmentService.appCreate(userId,testId,description,appointment_time);
+        String description = (String) AppMap.get("description");
+        Date appointment_time = (Date) AppMap.get("appTime");
+        int app = appointmentService.appCreate(userId, testId, description, appointment_time);
         return new ResponseEntity(app, HttpStatus.CREATED);
     }
 
 
 
-
+    @DeleteMapping("/id}")
+    public ResponseEntity<Map<String, Boolean>> deleteAppointment(HttpServletRequest request,@PathVariable("id") Integer appId) {
+        int userId = (Integer) request.getAttribute("userId");
+        appointmentService.appRemove(appId, userId);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("success", true);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
 
 
 }
